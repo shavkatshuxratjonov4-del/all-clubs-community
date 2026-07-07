7056c7c321cc0a08677427a69a31b4c4b0a35e61
Shavkatjon Shuxratjonov <shavkatshuxratjonov4@gmail.com>
2026-07-07 20:30:10 +0500

    fix: add placeholder pages

---
+++ app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function ContactPage() {
+  return (
+    <main className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">Contact Page</h1>
+    </main>
+  );
+}
---
+++ app/gallery/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-5xl font-bold">Coming Soon</h1>
+    </main>
+  );
+}
