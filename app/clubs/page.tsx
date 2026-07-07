5605af918973e8ff0fe4c438b788fca27256dfa8
Shavkatjon Shuxratjonov <shavkatshuxratjonov4@gmail.com>
2026-07-07 20:23:44 +0500

    fix: add clubs page export

---
+++ app/clubs/page.tsx
@@ -0,0 +1,7 @@
+export default function ClubsPage() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">Clubs Page</h1>
+    </div>
+  );
+}
